75a213c2ac29fbb30b11b66588b298cbecea7719
mertcan <dikmenmertcan@gmail.com>
2025-06-14 18:44:40 +0300

    kirksekizinci gun

---
+++ src/day48_abstractClasses_interface/C01_KuralciToyota.java
@@ -0,0 +1,30 @@
+
+package day48_abstractClasses_interface;
+
+
+
+public abstract class C01_KuralciToyota {
+
+
+    // oncelikle child class'larin kendisine uyarlamasi zorunlu olan method'lari yazalim
+
+    public abstract void motor();
+    // abstract method'lar klasik method'lardan ziyade
+    // standartlari belirleyen bir cumle gibidir
+    // 8.satirdaki method'u soyle turkcelestirebiliriz :
+    // C01 class'ini parent edinen her child class motor() bulundurmak zorundadir
+
+    public abstract void teker();
+
+
+    // child class'larin bulundurmasi mecbur olmayan method'lari
+    // concrete (abstract olmayan) method olarak olusturabiliriz
+
+    public void sunroof(){
+        System.out.println("Toyota araclarda sunroof opsiyoneldir");
+    }
+
+    public void xenonFar(){
+        System.out.println("Toyota araclarda xenon opsiyoneldir");
+    }
+}
---
+++ src/day48_abstractClasses_interface/C02_Auris.java
@@ -0,0 +1,54 @@
+
+package day48_abstractClasses_interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C02_Auris extends C01_KuralciToyota{
+
+
+    @Override
+    public void motor() {
+        System.out.println("Auris motor");
+    }
+
+    @Override
+    public void teker() {
+        System.out.println("Auris teker");
+
+    }
+
+
+    public static void main(String[] args) {
+
+        // C01_KuralciToyota toyota = new C01_KuralciToyota();
+        // 'C01_KuralciToyota' is abstract; cannot be instantiated
+        // Toyota class'i abstract oldugundan OBJE OLUSTURULAMAZ
+        // Cunku boyle bir obje olusturulsa
+        // Toyota class'indaki abstract motor() ve teker() method'lari
+        // calisamayacagi icin sorun olusur
+        // toyota.motor();
+        // toyota.teker();
+        // toyota.sunroof();
+        // toyota.xenonFar();
+
+
+        // List<String> liste = new List<>();
+        // 'List' is abstract; cannot be instantiated
+
+        List<String> liste = new ArrayList<>();
+        // List abstract oldugundan esitligin saginda kullanamayiz
+        // esitligin saginda List'in child'i olan ArrayList() kullandik
+
+
+        C01_KuralciToyota toyota1 = new C02_Auris();
+        toyota1.motor(); // Auris motor
+        toyota1.teker(); // Auris teker
+        toyota1.xenonFar(); // Toyota araclarda xenon opsiyoneldir
+        toyota1.sunroof(); // Toyota araclarda sunroof opsiyoneldir
+
+    }
+
+
+
+}
